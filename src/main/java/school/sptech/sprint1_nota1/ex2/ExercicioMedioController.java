35c70561963aaaa491233c378e901ce6343f9d8a
matheussaldanha22 <matheus.amaral@sptech.school>
2025-08-18 23:18:59 -0300

    finalizado

--- src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
@@ -1,13 +1,26 @@
 package school.sptech.sprint1_nota1.ex2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioMedioController {
 
     @GetMapping("/ex-02/{numero}")
-    public Boolean exercicioMedio(int numero) {
-        return null;
+    public Boolean exercicioMedio(@PathVariable int numero) {
+        Integer qtdDivisiveis = 0;
+
+        for(int i = 1; i <= numero; i ++){
+            if(numero %i == 0){
+                qtdDivisiveis++;
+            }
+        }
+
+        if(numero % 1 == 0 && numero % numero == 0 && qtdDivisiveis == 2){
+            return true;
+        }
+
+        return false;
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -4,11 +4,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
+        List<Integer> sequencia = new ArrayList<>();
+        int ultimaPosicao = 0;
+        int posicaoAtual = 1;
+
+        int enesimo = 0;
+        int soma = 0;
+
+        for (int i = 0; i <= n; i++) {
+            sequencia.add(ultimaPosicao);
+            soma += ultimaPosicao;
+
+            if (i == n) {
+                enesimo = ultimaPosicao;
+            }
+
+            int somaFibo = ultimaPosicao + posicaoAtual;
+            ultimaPosicao = posicaoAtual;
+            posicaoAtual = somaFibo;
+        }
+
+        return new ExercicioDificilResponse(enesimo, soma);
     }
+
 }
